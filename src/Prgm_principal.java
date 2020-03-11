3c9e68bc1249ccc2ac590474ba418cfbddbcf6c5
Yoan Van der Spurt <vanderspurt@unistra.fr>
2020-03-11 10:06:06 +0100

    Dossier propre

--- src/Prgm_principal.java
+++ src/Prgm_principal.java
@@ -1,8 +1,9 @@
 import business_intelligence.view.Windows;
 
 public class Prgm_principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello");
             new Windows().setVisible(true);
+            new business_intelligence.controler.CtrlCharacter();
         }
 }
