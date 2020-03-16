a1189889d407eac6e5cc94a07f17e975908e50e8
Yoan Van der Spurt <vanderspurt@unistra.fr>
2020-03-16 10:07:40 +0100

    Ajout de la DataBase

--- src/controller/CtrlCharacter.java
+++ src/controller/CtrlCharacter.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import model.Character;
 
 import weka.core.Attribute;
+import weka.core.Instances;
 
 
 public class CtrlCharacter {
@@ -22,8 +23,11 @@ public class CtrlCharacter {
 
     private static ArrayList<String> cigarets;
 
+    private static ArrayList<String> glasses;
+
     private static ArrayList<Attribute> attributes;
 
+    private static Instances dataBase;
 
     private static ArrayList<Character> listCharacter = new ArrayList<Character>();
     
@@ -46,10 +50,18 @@ public class CtrlCharacter {
         cigarets.add("Cigar");
         cigarets.add("None");
 
+        glasses=new ArrayList<>();
+        glasses.add("Solar Glasses");
+        glasses.add("Nerd Glasses");
+        glasses.add("None");
+
         attributes.add(new Attribute("Hats",hats));
         attributes.add(new Attribute("Beards",beards));
         attributes.add(new Attribute("Cigarets",cigarets));
+        attributes.add(new Attribute("Glasses",glasses));
 
+        dataBase = new Instances("Students",attributes,50);
+        dataBase.setClassIndex(dataBase.numAttributes()-1);
     }
     
     public static Character creationCharacter() {
